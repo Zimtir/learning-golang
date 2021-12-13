135fa07722701c625db9ef5d1d91b036592831ca
Marat Zimnurov <zimtir@mail.ru>
2021-12-13 21:53:36 +0000

    feat: replace time circle with degrees

--- 05.degree-time-circle/main.go
+++ 05.degree-time-circle/main.go
@@ -7,14 +7,15 @@ import (
 func main() {
 
 	var number int = parseNumber()
-	var time string = formatTime(number)
+	var time string = formatDegree(number)
 
 	fmt.Printf(time)
 }
 
-func formatTime(number int) string {
-	var numberHours int = number / 60
-	var numberMinutes int = number % 60
+func formatDegree(number int) string {
+	var minutes int = (number * 60) / 30
+	var numberHours int = minutes / 60
+	var numberMinutes int = minutes % 60
 	var time = fmt.Sprintf("It is %d hours %d minutes.", numberHours, numberMinutes)
 
 	return time
